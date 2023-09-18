7e3267d56d7a96fdd85dc80ee49b190c101960d9
mannu2979 <man.mohan.in1@gmail.com>
2023-09-18 19:22:35 +0530

    remove test comment for changes in repo

--- App.js
+++ App.js
@@ -2,7 +2,6 @@
 import { SafeAreaView, StyleSheet, Text, View, StatusBar, ScrollView } from 'react-native';
 import ServiceList from './src/screens/ServiceList';
 export default function App() {
-  console.log('re')
   return (
       <SafeAreaView>
         <StatusBar backgroundColor='red' />
